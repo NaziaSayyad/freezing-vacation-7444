2d7ac24afb392108b9fa125b3d0c7080f27664cc
mr-ashu <ashutosh15th@gmail.com>
2022-07-24 01:00:46 +0530

    By Ashutosh

---
+++ Bewakoof_web-final/scripts/footer.js
@@ -0,0 +1,157 @@
+function footer(){
+return `
+<div id="footer-bag">
+<div id="ylogo">
+
+ <img src="https://images.bewakoof.com/lib/icon/app-logo.png" alt="">
+</div>
+<div id="niche">
+
+
+<div class="footer">
+   
+    <div>
+        <h3>CUSTOMER SERVICE</h3>
+        <p>Contact Us</p>
+        <p>Track Order</p>
+        <p>Return Order</p>
+        <p>Cancel Order</p>
+    </div>
+    <div>
+        <h3>COMPANY</h3>
+        <p>About Us</p>
+        <p>We're Hiring</p>
+        <p>Term & Conditions</p>
+        <p>Privacy Policy</p>
+        <p>Blog</p>
+    </div>
+    <div>
+        <h3>CONTECT WITH US</h3>
+        <p>4.7M People Like This</p>
+        <p>1M Followers</p>
+         
+    </div>
+    <div>
+        <h3>KEEP UP TO DATE</h3>
+        <div id="khoj">
+            <input type="text" placeholder="Enter Email Id">
+            <button>SUBSCRIBE</button>
+        </div>
+         
+    </div>
+
+</div>
+  
+ <div class="footer">
+            <div>
+                <p>15 Day return policy</p>
+                <p>Cash on delivery</p>
+            </div>
+            <div>
+                <h3>DOWNLOAD THE APP</h3>
+                <div id="platstore">
+                    <a><img src="https://images.bewakoof.com/web/app_android_v1.png" alt=""></a>
+                    <a><img src="https://images.bewakoof.com/web/app_ios_v1.png" alt=""></a>
+                </div>
+            </div>
+            <div>
+            <h3 id="pmt">100% SECURE PAYMENT</h3>
+           
+                <img src="https://images.bewakoof.com/web/secure-payments-image.png" alt="">
+                
+             
+
+            </div>
+            <div></div>
+        </div>
+
+  <hr id="hr">
+  <div class="footer">
+    <div>
+        <h4>MEN'S CLOTHING</h4>
+        <p> Men's New Arrivals</p>
+        <p> Men's Half Sleeve T-Shirts</p>
+        <p> Men's Hoodies & Sweatshirts</p>
+        <p>Men's Long Sleeve T-shirts </p>
+        <p>Men's Printed T-shirts </p>
+        <p>Men's Plain T-shirts </p>
+        <p> Men's Vests</p>
+        <p>Men's Kurtas </p>
+        <p> Men's Combo T-Shirts</p>
+        <p> Men's Shirts</p>
+        <p> Men's Shirts</p>
+        <p>Men's Plus Size Store </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p>Men's Track Pants </p>
+        <p>Men's Pants </p>
+    </div>
+    <div>
+        <h4>WOMEN'S CLOTHING </h4>
+        <p> Women's Top Wea</p>
+        <p> Women's New Arrivals</p>
+        <p> Women's T-Shirts</p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+        <p> </p>
+    </div>
+    <div>
+        <h4>MOBILE COVERS</h4>
+        <p>Brands</p>
+        <p>Apple</p>
+        <p>Realme</p>
+        <p>Samsiung</p>
+        <p>Xiomi</p>
+        <p>Oneplus</p>
+        <p>Vivo</p>
+        <p>Oppo</p>
+    </div>
+    <div id="hhh">
+        <p>FANBOOK</p>
+        <p>OFFERS</p>
+        <p>SITMAP</p>
+    </div>
+
+
+
+  </div>
+  </div>
+</div>
+`
+
+
+
+}
+
+export {footer}
