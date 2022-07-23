39140572458bcd1fe67b60f6fafb82b99c5e3f95
Ashutosh Sharma <104469510+mr-ashu@users.noreply.github.com>
2022-07-23 07:24:51 +0530

    Ashutosh Sharma
    Day-4

---
+++ day-4/scripts/navbar.js
@@ -0,0 +1,204 @@
+function navbar(){
+    return `
+    <div id="navbar-box">
+    <div class="top">
+    <div class="header">
+        <div class="baya">
+            <a href="">Offers</a>
+            <a href="">Facebook</a>
+            <a href="">Download app</a>
+            <a href="">TriBe Membership</a>
+            
+        </div>
+         <div class="daya">
+             <a href="">Contact Us</a>
+             <a href="">Track Order</a>
+         </div>
+      </div>
+
+</div>
+
+<div class="jhola">
+  
+<div class="navbar">
+  
+      
+   
+  <div class="ul">
+    <a href="index.html">
+        <img src="https://images.bewakoof.com/web/ic-desktop-normal-bwkf-logo.svg" alt="">
+       </a> 
+    
+     <div class="li">MEN 
+       <div class="mega-menu">
+        <div class="inner-mega">
+            <div><h3>Topwear</h3>
+                <p>Printed T-shirt</p>
+                <p>Oversized T-shirt</p>
+                <p>Plain T-shirt</p>
+                <p>Fashion T-Shirt</p>
+                <p>Shirt</p>
+                <p>Vests</p>
+                <p>Co-ords Sets</p>
+                <p>Kurtas</p>
+                <p>Henleys</p>
+                <p>Polo T-Shirt</p>
+                <p>Jackets</p>
+                <p>Hoodie</p>
+                <p>All Topwear</p></div>
+            <div>
+                <h3>Bottomwear</h3>
+                <p>Joggers</p>
+                <p>Pajamas</p>
+                <p>Shorts</p>
+            </div>
+            <div>
+                <h3>Accessories</h3>
+                <p>Mobile covers</p>
+                <p>Phone Grippers</p>
+                <p>Cap</p>
+                <p>Masks</p>
+
+               
+            </div>
+            <hr>
+            <div class="logo">
+                <div><img src="https://images.bewakoof.com/nav_menu/pride-icon-circle-1655695905.png" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/go-local-icon-1654622393.png" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/coke-icon-1645251020.png" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/COSMOS-Bewakoof-Homepage-NavigationCircleIcon-1643089061.png" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/Circle-Icon-Marvel-Youth-Beat-1653558342.png" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/Friends-Collection-Icons-168x168-1644555932.png" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/Circle-icon-ribbed-collection-1652639811.png" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/Anime-Bewakoof-Homepage-NavigationCircleIcon-final-1632292125.jpg" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/arena-icon-1652290108.png" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/bewakoof-icon-specials-7-1622012421.png" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/bewakoof-icon-specials-7-1622012421.png" alt=""></div>
+                <div><img src="https://images.bewakoof.com/nav_menu/icon-regional-1652171530.png" alt=""></div>
+
+
+
+            </div>
+        </div>
+       </div>
+     </div>
+     <div class="li">WOMEN
+        <div class="mega-menu">
+            <div class="inner-mega">
+                <div><h3>Topwear</h3>
+                    <p>Printed T-shirt</p>
+                    <p>Oversized T-shirt</p>
+                    <p>Plain T-shirt</p>
+                    <p>Fashion T-Shirt</p>
+                    <p>Shirt</p>
+                    <p>Vests</p>
+                    <p>Co-ords Sets</p>
+                    <p>Kurtas</p>
+                    <p>Henleys</p>
+                    <p>Polo T-Shirt</p>
+                    <p>Jackets</p>
+                    <p>Hoodie</p>
+                    <p>All Topwear</p></div>
+                <div>
+                    <h3>Bottomwear</h3>
+                    <p>Joggers</p>
+                    <p>Pajamas</p>
+                    <p>Shorts</p>
+                </div>
+                <div>
+                    <h3>Accessories</h3>
+                    <p>Mobile covers</p>
+                    <p>Phone Grippers</p>
+                    <p>Cap</p>
+                    <p>Masks</p>
+
+                   
+                </div>
+                <hr>
+                <div class="logo">
+                    <div><img src="https://images.bewakoof.com/nav_menu/pride-icon-circle-1655695905.png" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/go-local-icon-1654622393.png" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/coke-icon-1645251020.png" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/COSMOS-Bewakoof-Homepage-NavigationCircleIcon-1643089061.png" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/Circle-Icon-Marvel-Youth-Beat-1653558342.png" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/Friends-Collection-Icons-168x168-1644555932.png" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/Circle-icon-ribbed-collection-1652639811.png" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/Anime-Bewakoof-Homepage-NavigationCircleIcon-final-1632292125.jpg" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/arena-icon-1652290108.png" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/bewakoof-icon-specials-7-1622012421.png" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/bewakoof-icon-specials-7-1622012421.png" alt=""></div>
+                    <div><img src="https://images.bewakoof.com/nav_menu/icon-regional-1652171530.png" alt=""></div>
+
+                </div>
+            </div>
+           </div>
+     </div>
+     <div class="li">MOBILE COVERS
+        <div class="mega-menu">
+            <div class="inner-mega">
+            <div>
+                <h3>Redmi</h3>
+                <p>Redmi 9 Power</p>
+                <p>Xiaomi Mi 10i</p>
+                <p>Xiaomi Mi 10i 5G</p>
+           </div>
+           <div>
+                <h3>Samsung</h3>
+                <p>Samsung Note 10</p>
+                <p>Samsung Note 10 lite</p>
+                <p>Samsung Note 9</p>
+                
+                </div>
+                <div>
+                <h3>OnePlus</h3>
+                <p>OnePlus 9R</p>
+                <p>OnePlus 9RT</p>
+                <p>OnePlus 9</p>
+                </div>
+                <div>
+                <h3>Realme</h3>
+                <p>Realme 5 Pro</p>
+                <p>Realme 5</p>
+                <p>Realme 9 Pro 5G</p>
+                </div>
+                <div>
+                <h3>Apple</h3>
+                <p>iPhone 13</p>
+                <p>iPhone 13</p>
+                <p>iPhone 13</p>
+                </div>
+                <div>
+                <h3>Vivo</h3>
+                <p>Vivo S1</p>
+                <p>Vivo Y51 2020</p>
+                <p>Vivo Y73</p>
+                </div>
+            </div>
+           </div>
+     </div>
+  </div>
+  <div class="right">
+    <input type="text" id="search" placeholder="Search by product, category or collection">
+    <div class="account">
+        <a href="login.html" id="apnd">Login</a>
+        <a href=""><i class="fa-solid fa-heart"></i></a>
+        <a href=""><i class="fa-solid fa-bag-shopping"></i></a>
+    </div>
+  </div>
+  
+
+</div>
+
+</div>
+</div>
+
+`
+
+
+
+}
+
+
+
+
+export {navbar}
